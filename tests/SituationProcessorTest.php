0ff2ae3e5835f608817604c8714378c5d8696289
Erik Hoogeboom <bigtree@29a.nl>
2022-06-04 16:05:07 +0200

    added stub SituationProcessorTest

---
+++ tests/SituationProcessorTest.php
@@ -0,0 +1,24 @@
+<?php
+use BrugOpen\Core\EventDispatcher;
+use BrugOpen\Db\Service\MemoryTableManager;
+use BrugOpen\Ndw\Service\SituationProcessor;
+use PHPUnit\Framework\TestCase;
+
+class SituationProcessorTest extends TestCase
+{
+
+    public function testProcessSingleNewSituation()
+    {
+        $tableManager = new MemoryTableManager();
+        $eventDispatcher = new EventDispatcher(null);
+        $situationProcessor = new SituationProcessor(null);
+        $situationProcessor->setTableManager($tableManager);
+        $situationProcessor->setEventDispatcher($eventDispatcher);
+    }
+
+    public function testProcessSingleUpdatedSituation()
+    {}
+
+    public function testProcessSnapshot()
+    {}
+}
