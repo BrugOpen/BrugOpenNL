b74d4151087d7d90fbb2c783afd32d01dad32381
Erik Hoogeboom <bigtree@29a.nl>
2024-07-15 21:00:17 +0200

    further implemented DefaultEventListener

--- src/BrugOpen/Core/DefaultEventListener.php
+++ src/BrugOpen/Core/DefaultEventListener.php
@@ -1,5 +1,9 @@
 <?php
+
 namespace BrugOpen\Core;
+
+use BrugOpen\Db\Service\DatabaseTableManagerFactory;
+use BrugOpen\Service\OperationDispatcher;
 
 class DefaultEventListener
 {
@@ -8,7 +12,7 @@ class DefaultEventListener
      *
      * @var \BrugOpen\Core\Context
      */
-    private $context;
+    protected $context;
 
     /**
      *
@@ -16,12 +20,35 @@ class DefaultEventListener
      */
     public function onContextInitialized($context)
     {
+
         $this->context = $context;
 
-        $context->getEventDispatcher()->addObserver('ServiceRegistry.initialize', array(
-            $this,
-            'onServiceRegistryInitialize'
-        ));
+        $eventDispatcher = $context->getEventDispatcher();
+
+        $eventHooks = $this->getEventHooks();
+
+        foreach ($eventHooks as $eventHook) {
+
+            $eventName = $eventHook[0];
+            $callback = $eventHook[1];
+
+            $eventDispatcher->addObserver($eventName, $callback);
+        }
+    }
+
+    public function getEventHooks()
+    {
+
+        $eventHooks = array();
+
+        $context = $this->context;
+
+        $operationDispatcher = new OperationDispatcher($context);
+
+        $eventHooks[] = array('ServiceRegistry.initialize', array($this, 'onServiceRegistryInitialize'));
+        $eventHooks[] = array('Operation.update', array($operationDispatcher, 'onOperationUpdate'));
+
+        return $eventHooks;
     }
 
     /**
@@ -32,6 +59,17 @@ class DefaultEventListener
     {
         $serviceFactory = new DefaultServiceFactory();
 
+        $serviceRegistry->registerServiceFactory('BrugOpen.BridgePassageCalculator', $serviceFactory);
+        $serviceRegistry->registerServiceFactory('BrugOpen.BridgeService', $serviceFactory);
+        $serviceRegistry->registerServiceFactory('BrugOpen.JourneyDataStore', $serviceFactory);
         $serviceRegistry->registerServiceFactory('BrugOpen.NdwQueueProcessor', $serviceFactory);
+        $serviceRegistry->registerServiceFactory('BrugOpen.OperationIndexService', $serviceFactory);
+        $serviceRegistry->registerServiceFactory('BrugOpen.PassageService', $serviceFactory);
+        $serviceRegistry->registerServiceFactory('BrugOpen.VesselPositionProcessor', $serviceFactory);
+        $serviceRegistry->registerServiceFactory('BrugOpen.WaterwayService', $serviceFactory);
+
+        $tableManagerFactory = new DatabaseTableManagerFactory();
+
+        $serviceRegistry->registerServiceFactory('BrugOpen.TableManager', $tableManagerFactory);
     }
 }
